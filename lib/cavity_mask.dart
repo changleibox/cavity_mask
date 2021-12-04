97552f44a776e8fae1282a12402d7bbb1b0b96c3
lei.chang <540739522@qq.com>
2021-12-04 14:03:23 +0800

    feat: 初始提交

--- lib/cavity_mask.dart
+++ lib/cavity_mask.dart
@@ -1,3 +1,4 @@
 library cavity_mask;
 
-export 'package:cavity_mask/src/basic.dart';
+export 'package:cavity_mask/src/basic.dart'
+    hide RenderCavityRect, RenderCavityRRect, RenderCavityOval, RenderCavityPath;
